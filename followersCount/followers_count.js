12334d75c890c82b812feb6c9597e86aac7234a1
devang2096 <dubey.devang2096@gmail.com>
2025-04-01 13:41:47 -0400

    reset added

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -15,10 +15,16 @@ function increaseCount() {
   }
 
   function checkCountValue() {
-    if (count === 10) {
+    if (count == 10) {
       alert("Your Instagram post gained 10 followers! Congratulations!");
-    } else if (count === 20) {
+    } else if (count == 20) {
       alert("Your Instagram post gained 20 followers! Keep it up!");
     }
+  }
+
+  function resetCount() {
+    count = 0;
+    displayCount();
+    alert("Followers count has been reset.")
   }
   
